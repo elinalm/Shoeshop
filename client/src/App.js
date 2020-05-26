d53c61ffc444dad64f555e7e9e69fb01ba205d95
mamta srinivas <mamtasrinivassp@gmail.com>
2020-05-26 08:01:36 +0200

    fixed Link

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import MyCart from './components/checkout/MyCart';
 import HomePage from './components/HomePage';
 import Checkout from './components/checkout/Checkout';
+import CategoriesPage from './components/CategoryPage'
 import ProductPage from './components/ProductPage';
 import UserProvider, { UserConsumer } from "./context/userContext";
 import ProductProvider, { ProductConsumer } from "./context/productContext";
@@ -17,8 +18,8 @@ function App() {
                         <Route path="/MyCart" component={MyCart} />
                         <Route path="/Checkout" component={Checkout} />
                         <Route path="/product/:id" component={ProductPage} />
+                        <Route path="/:category" component={HomePage} />
                         <Route path="/" component={HomePage} />
-                        {/* <Route path="/:category" component={CategoriesPage} /> */}
                     </Switch>
 
                 </UserProvider>
