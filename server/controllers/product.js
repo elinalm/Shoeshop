2f9834ba9de2a32f0f612fa76e410e501f4a73d2
emmamariagaby <emmamariagaby@gmail.com>
2020-05-26 19:51:22 +0200

    added content frame for products

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,54 +3,150 @@ const Product = require("../models/Product");
 var products = [
     // summer 
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "summer"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "summer"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "summer"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "summer"
     }),
     // street
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "street"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "street"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "street"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "street"
     }),
     // formal
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "formal"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "formal"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "formal"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "formal"
     }),
     // party
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "party"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "party"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "party"
     }),
     new Product({
-
+        imagePath: "",
+        brand: "",
+        description: "",
+        price: "",
+        size: "",
+        quantity: "",
+        category: "party"
     }),
 ]
