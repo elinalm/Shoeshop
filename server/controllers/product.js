3fe90ea1e7cbcbfe0e809dcb68f168a907066247
emmamariagaby <emmamariagaby@gmail.com>
2020-05-26 20:13:59 +0200

    updated content frame products

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -3,150 +3,202 @@ const Product = require("../models/Product");
 var products = [
     // summer 
     new Product({
+        category: "summer",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "summer"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "summer",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "summer"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "summer",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "summer"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "summer",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "summer"
+        quantity: ""
+        }
     }),
     // street
     new Product({
+        category: "street",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "street"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "street",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "street"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "street",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "street"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "street",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "street"
+        quantity: ""
+        }
     }),
     // formal
     new Product({
+        category: "formal",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "formal"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "formal",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "formal"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "formal",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "formal"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "formal",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "formal"
+        quantity: ""
+        }
     }),
     // party
     new Product({
+        category: "party",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "party"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "party",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "party"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "party",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "party"
+        quantity: ""
+        }
     }),
     new Product({
+        category: "party",
         imagePath: "",
         brand: "",
         description: "",
         price: "",
+        inventory: 
+        {
         size: "",
-        quantity: "",
-        category: "party"
+        quantity: ""
+        }
     }),
 ]
+// loops through and store products
+for (var i = 0; i < products.length; i++) {
+    products[i].save();
+}
