c427bd34bb7d7fbc6f7410e42821341c7878f187
emmamariagaby <emmamariagaby@gmail.com>
2020-05-26 19:44:49 +0200

    added frame for implement products for all seasons

---
+++ server/controllers/product.js
@@ -0,0 +1,56 @@
+const Product = require("../models/Product");
+
+var products = [
+    // summer 
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    // street
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    // formal
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    // party
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+    new Product({
+
+    }),
+]
