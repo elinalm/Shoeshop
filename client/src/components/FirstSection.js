f032cb9e8af6e8a7e3d05934b338c608d193e02b
Elin Alm <elin95alm@gmail.com>
2020-06-11 13:42:07 +0200

    Json-files added, changed carousel pictures. Overall done!

--- client/src/components/FirstSection.js
+++ client/src/components/FirstSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Box, Carousel, Image } from "grommet";
-import CarouselImage1 from '../assets/sports.jpg'
+import CarouselImage1 from '../assets/legs.jpg'
 import CarouselImage2 from '../assets/fancy.jpg'
-import CarouselImage3 from '../assets/street.jpg'
+import CarouselImage3 from '../assets/quote.jpg'
+import CarouselImage4 from '../assets/quote2.jpg'
 
 export default function FirstSection() {
     return (
@@ -16,9 +17,11 @@ export default function FirstSection() {
             pad='small'
         >
             <Carousel fill play={5000}>
-                <Image fit="cover"  style={{ width: '100%', height: '100%' }} src={CarouselImage1} />
-                <Image fit="cover"  style={{ width: '100%', height: '100%' }} src={CarouselImage2} />
+
                 <Image fit="cover"  style={{ width: '100%', height: '100%' }} src={CarouselImage3} />
+                <Image fit="cover"  style={{ width: '100%', height: '100%' }} src={CarouselImage2} />
+                <Image fit="cover"  style={{ width: '100%', height: '100%' }} src={CarouselImage1} />
+                <Image fit="cover"  style={{ width: '100%', height: '100%' }} src={CarouselImage4} />
             </Carousel>
         </Box>
 
